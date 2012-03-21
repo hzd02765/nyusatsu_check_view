ac5c4efb9b51789e6b4898cb32053356d29c8495
SakaiTomoyuki@ce50h7 <hzd02765@nifty.com>
2012-03-21 18:32:02 +0900

    commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8" />
+	<title>NYUSATSU_CHECK_VIEW</title>
+	<script src="./nyusatsu_check_view.js"></script>
+</head>
+<body>
+<h1>HEADER</h1>
+<div>
+	<p>案件TYPE</p>
+	<input type="radio" name="anken_type" value="">現在公開分 => 一般競争入札
+	<input type="radio" name="anken_type" value="">現在公開分 => 簡易公開調達
+	<input type="radio" name="anken_type" value="">既に終了分 => 一般競争入札
+	<input type="radio" name="anken_type" value="">既に終了分 => 簡易公開調達
+</div>
+<div>
+	<p>大分類</p>
+	<select>
+		<option></option>
+		<option></option>
+		<option></option>
+		<option></option>
+		<option></option>
+		<option></option>
+	</select>
+</div>
+<div>
+	<p>小分類</p>
+	<select>
+		<option></option>
+		<option></option>
+		<option></option>
+		<option></option>
+		<option></option>
+		<option></option>
+	</select>
+</div>
+<div>
+	<p>詳細</p>
+</div>
+<h2>FOOTER</h2>
+</body>
+</html>
