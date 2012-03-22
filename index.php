99478aaf41122c9d3b0107a4dc15e199b4abf12a
SakaiTomoyuki@ce50h7 <hzd02765@nifty.com>
2012-03-22 19:28:38 +0900

    commit

--- index.php
+++ index.php
@@ -3,105 +3,129 @@
 <head>
 	<meta charset="UTF-8" />
 	<title>NYUSATSU_CHECK_VIEW</title>
+	<link href="./css/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 	<script src="./js/jquery-1.7.2.min.js"></script>
 	<script src="./js/main.js"></script>
+	<style>
+		body{
+			background-color:FFFFFF;
+		}
+		h1{
+			// background-color:green;
+		}
+		h2{
+			// background-color:yellow;
+		}
+		div{
+			// background-color:blue;
+		}
+	</style>
 </head>
 <body>
-<h1>HEADER</h1>
-<div>
-	<p>案件TYPE</p>
-	<select id='kind'>
-		<option value=""></option>
-		<option value="keishu_cd=1&public_flag=0">現在公開分 => 一般競争入札</option>
-		<option value="keishu_cd=2&public_flag=0">現在公開分 => 簡易公開調達</option>
-		<option value="keishu_cd=1&public_flag=1">既に終了分 => 一般競争入札</option>
-		<option value="keishu_cd=2&public_flag=1">既に終了分 => 簡易公開調達</option>
-	</select>
+<div class="container">
+	<div class="hero-unit">
+		<h1>HEADER</h1>
+	</div>
+	<div class="row">
+		<div class="span3">
+			<p>案件TYPE</p>
+			<select id='kind' size="10">
+				<option value=""></option>
+				<option value="keishu_cd=1&public_flag=0">現在公開分 => 一般競争入札</option>
+				<option value="keishu_cd=2&public_flag=0">現在公開分 => 簡易公開調達</option>
+				<option value="keishu_cd=1&public_flag=1">既に終了分 => 一般競争入札</option>
+				<option value="keishu_cd=2&public_flag=1">既に終了分 => 簡易公開調達</option>
+			</select>
+		</div>
+		<div class="span3">
+			<p>大分類</p>
+			<select id='kbn1' size="10">
+			</select>
+		</div>
+		<div class="span3">
+			<p>小分類</p>
+			<select id='kbn2' size="10">
+			</select>
+		</div>
+		<div class="span3">
+			<p>案件</p>
+			<select id='anken' size="10">
+			</select>
+		</div>
+	</div>
+	<div class="row">
+		<div class="span16">
+		<p>詳細</p>
+		<ul>
+			<li>
+				id・・・<span id="anken_id"></span>
+			</li>
+			<li>
+				anken_no・・・
+				<span id="anken_no"></span>
+			</li>
+			<li>
+				nyusatsu_system・・・
+				<span id="anken_nyusatsu_system"></span>
+			</li>
+			<li>
+				nyusatsu_type・・・
+				<span id="anken_nyusatsu_type"></span>
+			</li>
+			<li>
+				keishu_cd・・・
+				<span id="anken_keishu_cd"></span>
+			</li>
+			<li>
+				public_flag・・・
+				<span id="anken_public_flag"></span>
+			</li>
+			<li>
+				gyoumu_kbn_1・・・
+				<span id="anken_gyoumu_kbn_1"></span>
+			</li>
+			<li>
+				gyoumu_kbn_2・・・
+				<span id="anken_gyoumu_kbn_2"></span>
+			</li>
+			<li>
+				anken_name・・・
+				<span id="anken_name"></span>
+			</li>
+			<li>
+				kasitu_name・・・
+				<span id="anken_kasitu_name"></span>
+			</li>
+			<li>
+				tender_date・・・
+				<span id="anken_tender_date"></span>
+			</li>
+			<li>
+				anken_url・・・
+				<span id="anken_url"></span>
+			</li>
+			<li>
+				version_no・・・
+				<span id="anken_version_no"></span>
+			</li>
+			<li>
+				delete_flag・・・
+				<span id="anken_delete_flag"></span>
+			</li>
+			<li>
+				ins_date・・・
+				<span id="anken_ins_date"></span>
+			</li>
+			<li>
+				upd_date・・・
+				<span id="anken_upd_date"></span>
+			</li>
+		</ul>
+		</div>
+	</div>
+	<footer>
+		<h2>FOOTER</h2>
+	</footer>
 </div>
-<div>
-	<p>大分類</p>
-	<select id='kbn1'>
-	</select>
-</div>
-<div>
-	<p>小分類</p>
-	<select id='kbn2'>
-	</select>
-</div>
-<div>
-	<p>案件</p>
-	<select id='anken'>
-	</select>
-</div>
-<div>
-	<p>詳細</p>
-	<ul>
-		<li>
-			id・・・
-			<label id="anken_id"></label>
-		</li>
-		<li>
-			anken_no・・・
-			<label id="anken_no"></label>
-		</li>
-		<li>
-			nyusatsu_system・・・
-			<label id="anken_id"></label>
-		</li>
-		<li>
-			nyusatsu_type・・・
-			<label id="anken_nyusatsu_system"></label>
-		</li>
-		<li>
-			keishu_cd・・・
-			<label id="anken_keishu_cd"></label>
-		</li>
-		<li>
-			public_flag・・・
-			<label id="anken_public_flag"></label>
-		</li>
-		<li>
-			gyoumu_kbn_1・・・
-			<label id="anken_gyoumu_kbn_1"></label>
-		</li>
-		<li>
-			gyoumu_kbn_2・・・
-			<label id="anken_gyoumu_kbn_2"></label>
-		</li>
-		<li>
-			anken_name・・・
-			<label id="anken_name"></label>
-		</li>
-		<li>
-			kasitu_name・・・
-			<label id="anken_kasitu_name"></label>
-		</li>
-		<li>
-			tender_date・・・
-			<label id="anken_tender_date"></label>
-		</li>
-		<li>
-			anken_url・・・
-			<label id="anken_url"></label>
-		</li>
-		<li>
-			version_no・・・
-			<label id="anken_version_no"></label>
-		</li>
-		<li>
-			delete_flag・・・
-			<label id="anken_delete_flag"></label>
-		</li>
-		<li>
-			ins_date・・・
-			<label id="anken_ins_date"></label>
-		</li>
-		<li>
-			upd_date・・・
-			<label id="anken_upd_date"></label>
-		</li>
-	</ul>
-</div>
-<h2>FOOTER</h2>
 </body>
 </html>
