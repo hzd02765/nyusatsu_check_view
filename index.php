31a204ec70e9fa9f6723aff7b7948ced289bee01
SakaiTomoyuki@ce50h7 <hzd02765@nifty.com>
2012-03-22 16:32:09 +0900

    commit

--- index.php
+++ index.php
@@ -11,11 +11,11 @@
 <div>
 	<p>案件TYPE</p>
 	<select id='kind'>
-		<option></option>
-		<option>現在公開分 => 一般競争入札</option>
-		<option>現在公開分 => 簡易公開調達</option>
-		<option>既に終了分 => 一般競争入札</option>
-		<option>既に終了分 => 簡易公開調達</option>
+		<option value="0"></option>
+		<option value="1">現在公開分 => 一般競争入札</option>
+		<option value="2">現在公開分 => 簡易公開調達</option>
+		<option value="3">既に終了分 => 一般競争入札</option>
+		<option value="4">既に終了分 => 簡易公開調達</option>
 	</select>
 </div>
 <div>
@@ -29,7 +29,30 @@
 	</select>
 </div>
 <div>
+	<p>案件</p>
+	<ul>
+	</ul>
+</div>
+<div>
 	<p>詳細</p>
+	<ul>
+		<li>id</li>
+		<li>anken_no</li>
+		<li>nyusatsu_system</li>
+		<li>nyusatsu_type</li>
+		<li>keishu_cd</li>
+		<li>public_flag</li>
+		<li>gyoumu_kbn_1</li>
+		<li>gyoumu_kbn_2</li>
+		<li>anken_name</li>
+		<li>kasitu_name</li>
+		<li>tender_date</li>
+		<li>anken_url</li>
+		<li>version_no</li>
+		<li>delete_flag</li>
+		<li>ins_date</li>
+		<li>upd_date</li>
+	</ul>
 </div>
 <h2>FOOTER</h2>
 </body>
--- js/main.js
+++ js/main.js
@@ -2,10 +2,40 @@ $(document).ready(function(){
 	$('#kind').change(function(){
 		// alert('a');
 		$('#kbn1').empty();
+		
 		$('#kbn1').append($('<option>').val('').text(''));
-		$('#kbn1').append($('<option>').val('a').text('a'));
-		$('#kbn1').append($('<option>').val('b').text('b'));
-		$('#kbn1').append($('<option>').val('c').text('c'));
-		$('#kbn1').append($('<option>').val('d').text('d'));
+		
+		// $('#kbn1').append($('<option>').val('a').text('a'));
+		// $('#kbn1').append($('<option>').val('b').text('b'));
+		// $('#kbn1').append($('<option>').val('c').text('c'));
+		// $('#kbn1').append($('<option>').val('d').text('d'));
+		
+		var type = $(this).val()
+		var data = '';
+		if(1 == type){
+			data = 'keishu_cd=1&public_flag=0';
+		}else if(2 == type){
+			data = 'keishu_cd=2&public_flag=0';
+		}else if(3 == type){
+			data = 'keishu_cd=1&public_flag=1';
+		}else if(4 == type){
+			data = 'keishu_cd=2&public_flag=1';
+		}else{
+			return true;
+		}
+		
+		$.ajax({
+			url: './ajax/get_kbn1.php',
+			type: 'POST',
+			data: data,
+			dataType: 'XML',
+			success: function(result){
+				$('Item', result).each(function(){
+					var value = $('KBN_CD', this).text();
+					var text = $('KBN_NAME', this).text();
+					$('#kbn1').append($('<option>').val(value).text(text));
+				});
+			}
+		});
 	});
 });
