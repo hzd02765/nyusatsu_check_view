bdb24d7d59f936971e79a85436428ded5a713384
tomoyuki@ce50h7 <tomoyuki@brains.info>
2013-01-17 14:53:58 +0900

    デバッグ用のコードを削除

--- js/main.js
+++ js/main.js
@@ -16,46 +16,6 @@ $(document).ready(function(){
 	}
 	// 詳細を空に
 	function emptyDetail(){
-/*
-		// 案件番号
-		$('#anken_no').text('');
-		// URL
-		$('#anken_url').text('');
-		// 案件名(事業年度・名称)
-		$('#anken_name').text('');
-		// 契約種別
-		$('#anken_keishu_name').text('');
-		// 対象業者の地域要件
-		$('#anken_company_area').text('');
-		// 公開開始日時
-		$('#anken_open_date').text('');
-		// 公開終了日時
-		$('#anken_close_date').text('');
-		// 入札日時
-		$('#anken_tender_date').text('');
-		// 入札場所
-		$('#anken_tender_place').text('');
-		// 履行期限
-		$('#anken_limit_date').text('');
-		// 業務大分類
-		$('#anken_gyoumu_kbn_1').text('');
-		// 業務小分類
-		$('#anken_gyoumu_kbn_2').text('');
-		// 実施機関
-		$('#anken_kasitu_name').text('');
-		// 担当者名・電話番号
-		$('#anken_tanto_name').text('');
-		// 特記事項
-		$('#anken_notes').text('');
-		// 結果表示開始日時
-		$('#anken_result_open_date').text('');
-		// 結果表示終了日時
-		$('#anken_result_close_date').text('');
-		// 落札業者名等
-		$('#anken_raku_name').text('');
-		// 落札金額（税込・円）
-		$('#anken_price').text('');
-*/
 		$('.anken-detail tr td span').text('');
 		$('.anken-detail tr td a').attr('href', '');
 		$('.anken-detail tr td a').text('');
@@ -68,8 +28,6 @@ $(document).ready(function(){
 	});
 	// 案件TYPEを選択
 	$('#kind').change(function(){
-		// $('#kbn1').empty();		
-		// $('#kbn1').append($('<option>').val('').text(''));
 		emptyKbn1();
 		emptyKbn2();
 		emptyAnken();
@@ -96,8 +54,6 @@ $(document).ready(function(){
 	});
 	// 大分類を選択
 	$('#kbn1').change(function(){
-		// $('#kbn2').empty();
-		// $('#kbn2').append($('<option>').val('').text(''));
 		emptyKbn2();
 		emptyAnken();
 		emptyDetail();
@@ -124,8 +80,6 @@ $(document).ready(function(){
 	});
 	// 小分類を選択
 	$('#kbn2').change(function(){
-		// $('#anken').empty();
-		// $('#anken').append($('<option>').val('').text(''));
 		emptyAnken();
 		emptyDetail();
 		
