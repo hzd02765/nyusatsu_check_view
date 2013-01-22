842f24e8e90e5c0059744a8f9b7e0e0f68550241
tomoyuki@ce50h7 <hzd02765@nifty.com>
2013-01-22 15:29:30 +0900

    issue-37 fixed

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('HOST', 'localhost');
+define('DBNAME', 'nyusatsu_check');
+define('USER', 'nyusatsu_check');
+define('PASSWORD', 'nyusatsu_check');
+ 
+// エラー表示
+ 
+// error_reporting(E_ALL & ~E_NOTICE);
+error_reporting(E_ALL);
